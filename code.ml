78d35b173cc685fc94055dcc69222fc38e42e951
Matthew Steedman <steedman87@gmail.com>
2017-07-10 21:47:26 -0400

    Open List module

--- code.ml
+++ code.ml
@@ -1,6 +1,4 @@
-(* ======= Util ======= *)
-
-let (<<) f g x = f(g(x));;
+open List
 
 (* ======= Point ======= *)
 
@@ -34,14 +32,14 @@ let is_alive = function
   | Dead _  -> false
 
 let neighbors cells cell =
-  List.filter (fun c -> List.mem (point_of c) (point_neighbors (point_of cell))) cells
+  filter (fun c -> mem (point_of c) (point_neighbors (point_of cell))) cells
 
 let next_cell cells cell =
   match cell with
-  | Alive _ -> if List.mem (List.length (List.filter is_alive (neighbors cells cell))) [2; 3]
+  | Alive _ -> if mem (length (filter is_alive (neighbors cells cell))) [2; 3]
     then Alive (point_of cell)
     else Dead (point_of cell)
-  | Dead _ -> if (List.length (List.filter is_alive (neighbors cells cell))) = 3
+  | Dead _ -> if (length (filter is_alive (neighbors cells cell))) = 3
     then Alive (point_of cell)
     else Dead (point_of cell)
 
@@ -60,7 +58,7 @@ let my_cells =
     Dead { x = 2; y = 2 };
   ]
 
-let my_cell = List.nth my_cells 4
+let my_cell = nth my_cells 4
 
 (* Test if my_cell should be alive on the next iteration *)
 let () = print_endline (string_of_bool (is_alive (next_cell my_cells my_cell)))
